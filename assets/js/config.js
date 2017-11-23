5ce267bbea2069a31fd295189e55964a0398f4b7
rchshrm16 <rchshrm16@gmail.com>
2017-11-23 13:51:07 +0530

    cover images size labeland validation

--- assets/js/config.js
+++ assets/js/config.js
@@ -332,7 +332,7 @@ angular.module('app')
         var live = "http://api.foodtalk.in/";
 
         // change before pushing online
-        var baseurl = stg;
+        var baseurl = live;
 
         UrlFact.appfeed = {};
         UrlFact.appfeed.redmption = baseurl + "privilege/feeds/redeemptions";
